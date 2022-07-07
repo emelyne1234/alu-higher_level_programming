b1bf0ce3a58bbdca1c66cbea8321f11783077e3c
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-07 00:13:30 -0700

    alphabet

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(1, 27)
+for letter in range(97, 123)
 if character(letter) ! = 'q' and character(letter) ! = 'e':
     print("{}".format(chr(letter)), end="")
