aa80264333c8318ad52e94b4ea066ac669e98121
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-08 04:11:42 -0700

    hidden talent

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
-    for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_'
-        print(name)
+    import hidden_4 as extra
+
+
+for x in dir(extra):
+    if x[0:2] != "__":
+        print("{:s}".format(x))
