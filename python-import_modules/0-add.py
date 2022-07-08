0e6990f6ecaa84386d6679fb8cbfce7263c8a4ac
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-08 01:20:39 -0700

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     form add_0 import add
-    a=1
-    b=2
-    numb=add(a, b)
+    a = 1
+    b = 2
+    numb = add(a, b)
     print("{} + {} = {}".format(a, b, numb))
