870e688cc0ad1f37e9989d1f241552f598191c53
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-08 04:16:36 -0700

    args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    
-    values = sys.argv
-    i = len(values) - 1
 
-    if i == 0:
-    print("{} arguments".format(i))
-elif i == 1:
-    print("{} arguments".format(i))
-else:
-    print("{} arguments".format(i))
 
-    if i >= 1:
-        i = 0
-        for arg in values:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-                i +=  1
+values = sys.argv
+number = len(values)
+i = 1
+summ = 0
+if number == 1:
+    print("{:d}".format(summ))
+elif number == 2:
+    print("{:d}".format(int(values[1])))
+elif number > 2:
+    while i < number:
+        summ = summ + int(values[i])
+        i = i + 1
+    print("{:d}".format(summ))
