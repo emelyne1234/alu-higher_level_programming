689928eedaf4c240a226242712e40de2e201e8aa
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-08 03:00:13 -0700

    args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    
+    values = sys.argv
+    number = len(values)
+    i = len(values) - 1
+
+    if i == 0:
+    print("{} arguments".format(i))
+elif i == 1:
+    print("{} arguments".format(i))
+else:
+    print("{} arguments".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in values:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+                i = i + 1
