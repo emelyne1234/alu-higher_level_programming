5cdb18d929ea15266154d003366fc090fae4596b
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-13 06:20:34 -0700

    replace in a copy

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+# !/usr/bin/python3
+# replacing an element in a list at a specific position
+
+def new_in_list(my_list, idx, elements):
+    if idx < 0 or idx >= len(my_list):
+        return copy(my_list)
+    else:
+        my_list[idx] = element
+        return my_list
