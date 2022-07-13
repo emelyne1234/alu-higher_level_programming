47d5c40b829055022fc54f5e75a5ec9e84c7e87c
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-13 06:38:09 -0700

    copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 # replacing an element in a list at a specific position
 
+
 def new_in_list(my_list, idx, elements):
     if idx < 0 or idx >= len(my_list):
-        return copy(my_list)
+        copy_it = my_list[:]
+        return copy_it
     else:
-        my_list[idx] = element
-        return my_list
+        copy_em = my_list[:]
+        copy_em[idx] = element
+        return copy_em
