74a833c9749552b17891eb52d6506150ea1ea7dc
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-13 03:04:49 -0700

    replace an element

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-# replacing in a list
-def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    elif idx >= range(len(my_list)):
-        return my_list
+# replacing an element
+if idx < 0 or idx > len(my_list):
+    return my_list
+else:
+    return my_list[idx, element]
