b07c8b7d141398a754c5bce6ff3a16c793dae941
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-13 02:35:54 -0700

    retrieve

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 # retrieving an element from a list
 def element_at(my_list, idx):
     if idx < 0:
-       return 
-   elif idx >= range(len(my_list)):
-       return 
-   else:
-       return my_list[idx]
+         return 
+    elif idx >= range(len(my_list)):
+         return 
+    else:
+         return my_list[idx]
