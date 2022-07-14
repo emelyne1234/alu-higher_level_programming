1808ccbcf689b35873ab80a58ffc6b5c93a8d354
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-14 10:04:02 -0700

    no c

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# a function that removes all characters from a string
+
+
+def no_c(my_string):
+    temp = ' '
+
+    for i in my_string:
+        if i.lower() == 'c':
+            continue
+        temp += i
+        return temp
