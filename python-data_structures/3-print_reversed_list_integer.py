cd0b296ab401f753284d4038e7e762b9b8478dd7
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-14 15:34:50 -0700

    print reversely

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -7,5 +7,5 @@ def print_reversed_list_integer(my_list=[]):
         count = len(my_list) - 1
         if count >= 0:
             for item in my_list:
-                print("{:d}".format(item))
+                print("{:d}".format(my_list[count])
                 count -= 1
