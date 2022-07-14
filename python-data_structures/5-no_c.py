b4f30dab9b54aaa70414cf51479e231a7e8d41e5
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-14 14:46:50 -0700

    remove c

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -6,7 +6,7 @@ def no_c(my_string):
     temp = ' '
 
     for i in my_string:
-        if i.lower() == 'c':
+        if (i.lower()) == 'c':
             continue
         temp += i
         return temp
