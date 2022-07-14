431ca45f41882a04fa27db53faa5d016306ae6bb
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-14 14:42:28 -0700

    delete_at.py

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    temp_list = list()
-    for temp in my_list:
-        if temp % 2 == 0:
-            temp_list.append(True)
-        else:
-            temp_list.append(False)
-    return temp_list
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        del my_list[idx]
+        return my_list
