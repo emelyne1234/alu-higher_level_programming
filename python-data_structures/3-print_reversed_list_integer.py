5877f4fae5c19f58428d76585bd6f3ab2bcd380b
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-14 16:11:20 -0700

    print reversely

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-# printing reversely
-
-
 def print_reversed_list_integer(my_list=[]):
-    if isinstance(my_list, list):
-        count = len(my_list) - 1
-        if count >= 0:
-            for item in my_list:
-                print("{:d}".format(item)
-                count -= 1
+    if not my_list:
+        pass
+    else:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
