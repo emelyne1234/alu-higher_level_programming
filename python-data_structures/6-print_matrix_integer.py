c19ed7996c7e5e698f869294fa90e3fbb262ea2d
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-14 10:17:17 -0700

    print lists of matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# printing matrix
+
+def print_matrix_integer(matrix=[[]]):
+    for record in matrix:
+        counter = 0
+        for item in record:
+            if counter == (len(record) - 1):
+                print("{:d}".format(item), end="")
+            else:
+                print("{:d}".format(item), end=" ")
+            counter += 1
+        print()
