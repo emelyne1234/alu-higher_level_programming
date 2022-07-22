fcbac1d16c2cc4120e621885cb6af554a1c3a48a
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-21 23:39:54 -0700

    divide a list

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# a function that divides element by element 2 lists
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    new_list=[]
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except TypeError:
+            print("wrong type")
+            reult = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            new_list.append(result)
+
+    return new_list
