3735b53102c2ad599e783aeeaff1b21952c1b6d5
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-22 05:01:04 -0700

    square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""
+Documentation for modules are tricky
+"""
+class square:
+    """
+    class create a private instance variable and assigns it
+    """
+    def __init__(self, size):
+        self.Square_size = size
+
