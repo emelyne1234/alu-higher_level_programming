283cb7173a52bdcaef4d4aa64acc0d0e3892481b
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-21 23:19:40 -0700

    integers division with debug

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@
 
 def safe_print_division(a, b):
     try:
-        result = (a,b)
+        result = (a/b)
     except (ValueError, RuntimeError, TypeError, ZeroDivisionError):
         return None
     finally:
