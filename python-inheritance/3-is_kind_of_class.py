cab1f8221422c107a0864fe3c79b7f35e5668c32
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-07-29 02:51:01 -0700

    same class or inheritance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" no module used """
+
+
+def is_kind_of_class(obj, a_class):
+    """ function returns true for any existence of obj by its own or from relatives """
+    return isinstance(obj, a_class)
+
