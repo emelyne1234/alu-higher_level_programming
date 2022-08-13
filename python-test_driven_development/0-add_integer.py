cd9441e661bd6ac56bcb4b92bf3b385cb96da854
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-08-13 04:46:04 -0700

    addition of integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,7 +3,7 @@
 
 
 def add_integer(a, b=98):
-    """ a function adds integers """
+    """ a function returns the addition of integers """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
