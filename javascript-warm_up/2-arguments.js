1a4d1e15dc8135c253dfeccb57e9c1654d565b3b
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-10-10 06:10:31 -0700

    arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = 3;
+if (args.length == 2) {
+console.log('Argument found')
+} else if (args.length > 2) {
+console.log('Argumentsfound')
+} else {
+console.log('No argument')
+}
