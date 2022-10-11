9dc62cb458f18e2121809d2a2defda4295c130a5
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-10-11 08:41:15 -0700

    try

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
-function numbering(number) {
-console.log('My number: 'number);
+const args = process.argv;
+const conv = number(args[0]);
+if (conv) {
+	console.log('My number: args[0]);
+else {
+	console.log('Not a number');
 }
-numbering(23);
