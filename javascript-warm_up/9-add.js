447a2a27c6695415336842ed2f068f2702c5d59e
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-10-14 03:57:17 -0700

    add

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
-const args = process.argv;
-const conA = Number(args[2]);
-const conB = Number(args[3]);
-
+const args = process.argv.slice(2);
+// prints the addition of 2 integers
 function add (a, b) {
   console.log(a + b);
 }
-
-if (conA && conB) {
-  add(conA, conB);
-} else {
-  console.log('NaN');
-}
+add(Number(args[0]), Number(args[1]));
