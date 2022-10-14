4944137d59615d827cefc8d0e79559ed125acf3b
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-10-14 03:52:26 -0700

    args

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const args = process.argv;
 const conv = number(args[2]);
 if (conv) {
-	console.log('My number: ${args[2]}');
+	console.log(`My number: ${args[2]}`);
 else {
 	console.log('Not a number');
 }
