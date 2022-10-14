35dfcf5bee2646852c6b297e9431fc56606211e6
emelyne1234 <e.uwimbabazi@alustudent.com>
2022-10-14 04:03:46 -0700

    args

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const args = argv.slice(2);
-if (args.length === 1) {
-console.log('Argument found');
+// A script that prints a message depending of the number of arguments passed
+
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
 } else if (args.length > 1) {
-console.log('Arguments found');
-} else {
-console.log('No argument');
+  console.log('Arguments found');
 }
